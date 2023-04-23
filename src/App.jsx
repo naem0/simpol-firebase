1ecd1850edd7b8422b92edf4d9a91ff7a5afef95
MD Naem <mmnayeem408@gmail.com>
2023-04-23 21:28:05 +0600

    email password section add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut, GithubAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut, GithubAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 import './App.css'
 import app from './firebase/firebase.config';
 
@@ -9,46 +9,82 @@ const googleProibadar = new GoogleAuthProvider()
 const githabProvider = new GithubAuthProvider();
 
 function App() {
-  const [usar, setUsar]= useState(null)
-  const hendalGoogleSineing= ()=>{
+  const [usar, setUsar] = useState(null)
+  const hendalGoogleSineing = () => {
     signInWithPopup(auth, googleProibadar)
-    .then(result =>{
-      const logeuser =result.user ;
-      console.log(logeuser)
-      setUsar(logeuser)
+      .then(result => {
+        const logeuser = result.user;
+        console.log(logeuser)
+        setUsar(logeuser)
 
-    })
-    .catch(error=>{
-      console.log(error)
-    })
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
-  const hendalgithabSineing= ()=>{
+  const hendalgithabSineing = () => {
     signInWithPopup(auth, githabProvider)
-    .then(result =>{
-      const logeuser =result.user ;
-      console.log(logeuser)
-      setUsar(logeuser)
+      .then(result => {
+        const logeuser = result.user;
+        console.log(logeuser)
+        setUsar(logeuser)
 
-    })
-    .catch(error=>{
-      console.log(error)
-    })
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
-  const hendalSingOut= ()=>{
+  const hendalSingOut = () => {
     signOut(auth)
-    .then(()=>{
-      setUsar(null)
-      console.log('logout')
-    })
-    .catch((error)=>{
-      console.error(error);
-    })
-    
+      .then(() => {
+        setUsar(null)
+        console.log('logout')
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+
+  }
+
+  const hendalSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const name = form.name.value;
+    console.log(email, password, name)
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+        setUsar(user)
+        // ...
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        console.log('errorMessage')
+
+      });
   }
   return (
     <div className="App">
       <div>
-        
+        <form onSubmit={hendalSubmit}>
+          <div className="container">
+            <label ><b>Name</b></label>
+            <input type="text" placeholder="Enter name" name="name" required />
+            <label ><b>Email</b></label>
+            <input type="email" placeholder="Enter Email" name="email" required />
+
+            <label ><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required />
+
+            <button className='submitbtn' type="submit">Login</button>
+
+          </div>
+        </form>
       </div>
       <h1>Vite + React</h1>
       <button onClick={hendalGoogleSineing}>google sing ing</button>
@@ -63,7 +99,7 @@ function App() {
         </div>
 
       }
-        
+
     </div>
   )
 }
